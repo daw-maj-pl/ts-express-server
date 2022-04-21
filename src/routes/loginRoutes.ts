c31d303e9fa05a9ab7a1ef5ae434226e4b4ddfac
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-04-21 08:56:29 +0200

    Move over GET /logout handler logic to getLogout and apply get decorator to that getLogout handler

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -34,11 +34,6 @@ router.get('/', (req: Request, res: Response) => {
   }
 });
 
-router.get('/logout', (req: Request, res: Response) => {
-  req.session = undefined;
-  res.redirect('/');
-});
-
 router.get('/protected', requireAuth, (req: Request, res: Response) => {
   res.send('Welcome to protected route, logged in user');
 });
